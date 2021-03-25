6d5998a3b794aea09406e843c6b2d718b1685a90
rafominas <81387697+rafominas@users.noreply.github.com>
2021-03-25 22:16:25 +0000

    optimizing my code

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const name = ["Andrew Hendrixson","Coco Chanel","Albert Einstein","Brian Tracy"];
+const messages = ["“Anyone who has ever made anything of importance was disciplined.” — ", "“Don’t spend time beating on a wall, hoping to transform it into a door.” — ", 
+"“Optimism is the one quality more associated with success and happiness than any other.” — ", 
+"“Creativity is intelligence having fun.” — ", "“Always keep your eyes open. Keep watching. Because whatever you see can inspire you.”"];
+const temp = [["Andrew Hendrixson","“Anyone who has ever made anything of importance was disciplined.” — "], ["Coco Chanel","“Don’t spend time beating on a wall, hoping to transform it into a door.” — "],
+["Albert Einstein","“Optimism is the one quality more associated with success and happiness than any other.” — "], []];
+
 function randomize (num1, num2){
-    let num3;
-    num3 = Math.floor(Math.random() * (num2 - num1 + 1) + num1);
-    
-    switch (num3){
-        case 1: ""
-        case 2:
-    }
-console.log(num3);
-
-
-
+    return Math.floor(Math.random() * (num2 - num1) + num1);
 };
-randomize(0, 5);
+
+
+function getMessage (randomNumber){
+     console.log(messages[randomNumber] + name[randomNumber]);
+};
+
+const randomNumber = randomize(0, messages.length);
+getMessage(randomNumber);
