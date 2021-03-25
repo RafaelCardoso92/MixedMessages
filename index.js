d8cce5c2c1d75480c30e980f086cf8cc46c85e53
rafominas <81387697+rafominas@users.noreply.github.com>
2021-03-25 21:07:11 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function randomize (num1, num2){
+    let num3;
+    num3 = Math.floor(Math.random() * (num2 - num1 + 1) + num1);
+    
+    switch (num3){
+        case 1: ""
+        case 2:
+    }
+console.log(num3);
+
+
+
+};
+randomize(0, 5);
