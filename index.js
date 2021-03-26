e02885d1f9d517aa1a70e935cb4daaf84d47b582
rafominas <81387697+rafominas@users.noreply.github.com>
2021-03-26 19:05:36 +0000

    added objects to better describe the program, code optimization

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const name = ["Andrew Hendrixson","Coco Chanel","Albert Einstein","Brian Tracy"];
-const messages = ["“Anyone who has ever made anything of importance was disciplined.” — ", "“Don’t spend time beating on a wall, hoping to transform it into a door.” — ", 
-"“Optimism is the one quality more associated with success and happiness than any other.” — ", 
-"“Creativity is intelligence having fun.” — ", "“Always keep your eyes open. Keep watching. Because whatever you see can inspire you.”"];
-const temp = [["Andrew Hendrixson","“Anyone who has ever made anything of importance was disciplined.” — "], ["Coco Chanel","“Don’t spend time beating on a wall, hoping to transform it into a door.” — "],
-["Albert Einstein","“Optimism is the one quality more associated with success and happiness than any other.” — "], []];
+const messages = [ 
+    {
+        author: "Andrew Hendrixson", 
+        message: "“Anyone who has ever made anything of importance was disciplined.” — "
+    },
+    {
+        author: "Coco Chanel", 
+        message: "“Don’t spend time beating on a wall, hoping to transform it into a door.” —"
+    },
+    {
+        author: "Albert Einstein", 
+        message: "“Optimism is the one quality more associated with success and happiness than any other.” — "
+    },
+    {
+        author: "Brian Tracy", 
+        message: "“Creativity is intelligence having fun.” — "
+    },
+];
 
 function randomize (num1, num2){
     return Math.floor(Math.random() * (num2 - num1) + num1);
 };
 
-
 function getMessage (randomNumber){
-     console.log(messages[randomNumber] + name[randomNumber]);
+     console.log(messages[randomNumber].message + messages[randomNumber].author);
 };
 
 const randomNumber = randomize(0, messages.length);
